94e1c1f20c28fd9abb8af964e206f58cfa960484
devsheder <lasselle.a@sfeir.com>
2020-08-13 19:39:16 +0200

    feat: add material-ui and sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,24 @@
 <script>
-
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
 </script>
 
-<h1>Welcome to Place My Friends</h1>
+<style>
+  @import "@material/typography/mdc-typography";
+
+  .container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<div class="container">
+  <TopAppBar variant="static" dense={false} color={'primary'}>
+    <Row>
+      <Section>
+        <IconButton class="material-icons">menu</IconButton>
+        <Title>Place My Friends</Title>
+      </Section>
+    </Row>
+  </TopAppBar>
+</div>
